2b96565d532a39aa63397a59bfdbd0287040f43d
Megan Howell <62813469+cyberqueenmeg@users.noreply.github.com>
2024-10-01 08:38:47 -0700

    topic 2

--- Topic 2/ConsoleApplication1.c
+++ Topic 2/ConsoleApplication1.c
@@ -1,10 +1,9 @@
 void function(int a) { 
 	return a + 1; 
 }  
+void function2(int a, int b);
 
-void function2(int a, int b);  void main() {
-	function(9);
+void main() {
+	function(9);     
 	function2(1,2); 
-}
-
-// cl /Os /Fa .\ConsoleApplication1.c
+} 
